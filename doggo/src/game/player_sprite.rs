4320f5f956f75b56b5bfcca7a9f3d0551c927d95
Antti Lehtonen <anttileh@gmail.com>
2024-11-22 16:10:59 +0200

    Animation seems to work.

--- doggo/src/game/player_sprite.rs
+++ doggo/src/game/player_sprite.rs
@@ -1,8 +1,8 @@
 use bevy::input::common_conditions::input_just_pressed;
+use bevy::input::ButtonInput;
 use bevy::prelude::*;
 use bevy_rapier2d::prelude::*;
 use std::time::Duration;
-use bevy::input::ButtonInput;
 
 use super::constants::{SPRITESHEET_COLS, SPRITESHEET_ROWS, SPRITE_TILE_HEIGHT, SPRITE_TILE_WIDTH};
 
@@ -113,40 +113,98 @@ pub fn trigger_animation(
 ) {
     for (mut animation, mut texture_atlas) in query.iter_mut() {
         if input.pressed(KeyCode::ArrowRight) {
-            println!("Walking right");
             texture_atlas.index = WALKING_RIGHT_SPRITE_FIRST_INDEX;
-            // animation.frame_timer = AnimationConfig::timer_from_fps(animation.fps);
+            animation.frame_timer = AnimationConfig::timer_from_fps(animation.fps);
         } else if input.pressed(KeyCode::ArrowLeft) {
-            println!("Walking left");
             texture_atlas.index = WALKING_LEFT_SPRITE_FIRST_INDEX;
-            //animation.frame_timer = AnimationConfig::timer_from_fps(animation.fps);
+            animation.frame_timer = AnimationConfig::timer_from_fps(animation.fps);
         } else {
             texture_atlas.index = STANDING_SPRITE_FIRST_INDEX;
         }
     }
 }
 
+// create method that returns the current state of the animation
+fn get_current_state(atlas: &TextureAtlas) -> String {
+    if atlas.index >= 1 && atlas.index <= 3 {
+        return "standing".to_string();
+    } else if atlas.index >= 1 + SPRITESHEET_COLS as usize
+        && atlas.index <= 3 + SPRITESHEET_COLS as usize
+    {
+        return "walking_left".to_string();
+    } else if atlas.index >= 1 + SPRITESHEET_COLS as usize * 2
+        && atlas.index <= 3 + SPRITESHEET_COLS as usize * 2
+    {
+        return "walking_right".to_string();
+    }
+    return "standing".to_string();
+}
+
+// Generate get_last_sprite_index
+fn get_last_sprite_index(state: &str) -> usize {
+    if state == "standing" {
+        return 3;
+    } else if state == "walking_left" {
+        return 3 + SPRITESHEET_COLS as usize;
+    } else if state == "walking_right" {
+        return 3 + SPRITESHEET_COLS as usize * 2;
+    }
+    return 3;
+}
+
+// Generate get_first_sprite_index
+fn get_first_sprite_index(state: &str) -> usize {
+    return get_last_sprite_index(state) - 2;
+}
+
 // This system loops through all the sprites in the `TextureAtlas`, from  `first_sprite_index` to
 // `last_sprite_index` (both defined in `AnimationConfig`).
 pub fn execute_animations(
-  time: Res<Time>,
-  mut query: Query<(&mut AnimationConfig, &mut TextureAtlas)>,
+    time: Res<Time>,
+    input: Res<ButtonInput<KeyCode>>,
+    mut query: Query<(&mut AnimationConfig, &mut TextureAtlas)>,
 ) {
-  for (mut config, mut atlas) in &mut query {
-      // we track how long the current sprite has been displayed for
-      config.frame_timer.tick(time.delta());
+    for (mut config, mut atlas) in &mut query {
+        // we track how long the current sprite has been displayed for
+        config.frame_timer.tick(time.delta());
+        // If it has been displayed for the user-defined amount of time (fps)...
+        if config.frame_timer.just_finished() {
+            let current_state = get_current_state(&atlas);
+            let mut next_state = current_state.clone();
 
-      // If it has been displayed for the user-defined amount of time (fps)...
-      if config.frame_timer.just_finished() {
-          if atlas.index == config.last_sprite_index {
-              // ...and it IS the last frame, then we move back to the first frame and stop.
-              atlas.index = config.first_sprite_index;
-          } else {
-              // ...and it is NOT the last frame, then we move to the next frame...
-              atlas.index += 1;
-              // ...and reset the frame timer to start counting all over again
-              config.frame_timer = AnimationConfig::timer_from_fps(config.fps);
-          }
-      }
-  }
+            // Check if the user is pressing the left or right arrow keys
+            if input.pressed(KeyCode::ArrowRight) {
+                // Set the animation configuration for walking right
+                next_state = "walking_right".to_string();
+            } else if input.pressed(KeyCode::ArrowLeft) {
+                // Set the animation configuration for walking left
+                next_state = "walking_left".to_string();
+            } else {
+                next_state = "standing".to_string();
+            }
+            let config_changed = current_state != next_state;
+
+            if config_changed {
+                /*
+                println!(
+                    "Config changed from {} to {}, atlas index: {}",
+                    current_state, next_state, atlas.index
+                );
+                */
+                atlas.index = get_first_sprite_index(&next_state);
+            }
+            if atlas.index == get_last_sprite_index(&next_state) {
+                // ...and it IS the last frame, then we move back to the first frame and stop.
+                atlas.index = get_first_sprite_index(&next_state);
+            } else {
+                // ...and it is NOT the last frame, then we move to the next frame...
+                atlas.index += 1;
+            }
+            // ...and reset the frame timer to start counting all over again
+            if next_state != "standing" {
+                config.frame_timer = AnimationConfig::timer_from_fps(config.fps);
+            }
+            // println!("Atlas index: {}", atlas.index);
+        }
+    }
 }
