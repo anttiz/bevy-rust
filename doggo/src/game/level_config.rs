3a078a143b17a8fcefc798fcbe1dcabdbbc6672b
Antti Lehtonen <anttileh@gmail.com>
2024-11-24 13:04:49 +0200

    Cool stuff.

--- doggo/src/game/level_config.rs
+++ doggo/src/game/level_config.rs
@@ -3,43 +3,43 @@ use std::sync::Mutex;
 // Add this at the top of the file to import the lazy_static crate
 use lazy_static::lazy_static; // Import lazy_static
 
+const STARTING_LEVEL: usize = 2;
 // Define a global variable for the current level
 lazy_static! {
-    pub static ref CURRENT_LEVEL: Mutex<usize> = Mutex::new(0); // Initialize to level 0
+    pub static ref CURRENT_LEVEL: Mutex<usize> = Mutex::new(STARTING_LEVEL); // Initialize to level 0
 }
 
 pub struct LevelConfig {
-    pub name: &'static str,         // Name of the level as a string slice
-    pub index: usize,                // Index of the level
-    pub stone_count: usize,          // Amount of stones in the level
-    pub stones_moving: bool,         // Boolean if stones are moving
-    pub stone_interval: f32,          // Stone interval in pixels
+    pub name: &'static str,  // Name of the level as a string slice
+    pub index: usize,        // Index of the level
+    pub stone_count: usize,  // Amount of stones in the level
+    pub stones_moving: bool, // Boolean if stones are moving
+    pub stone_interval: f32, // Stone interval in pixels
 }
 
 pub const LEVELS: &[LevelConfig] = &[
     LevelConfig {
-        name: "Level 1",         // Use a string literal
+        name: "Level 1",
         index: 0,
-        stone_count: 1,         // Amount of stones
-        stones_moving: false,     // Stones are moving
-        stone_interval: 50.0,    // Stone interval in pixels
+        stone_count: 1,
+        stones_moving: false,
+        stone_interval: 50.0,
     },
     LevelConfig {
-      name: "Level 2",         // Use a string literal
-      index: 1,
-      stone_count: 2,         // Amount of stones
-      stones_moving: false,     // Stones are moving
-      stone_interval: 100.0,    // Stone interval in pixels
-  },
-
-  LevelConfig {
-    name: "Level 3",         // Use a string literal
-    index: 2,
-    stone_count: 3,         // Amount of stones
-    stones_moving: false,     // Stones are moving
-    stone_interval: 100.0,    // Stone interval in pixels
-},
-  ];
+        name: "Level 2",
+        index: 1,
+        stone_count: 2,
+        stones_moving: false,
+        stone_interval: 400.0,
+    },
+    LevelConfig {
+        name: "Level 3",
+        index: 2,
+        stone_count: 2,
+        stones_moving: false,
+        stone_interval: 350.0,
+    },
+];
 
 // Example function to set the current level
 pub fn set_current_level(level_index: usize) {
